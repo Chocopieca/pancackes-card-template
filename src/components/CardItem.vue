96bbb8b411e18424d1cd91c22ebb8950c87c7c70
Kopyl Maksym <m.s.kopyl.kharkov@gmail.com>
2023-07-22 17:21:35 +0300

    study: created cards.

---
+++ src/components/CardItem.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="wrapper-block main-dark-purple border main-green-border overflow-hidden">
+    <div class="main-light-purple d-flex justify-space-between pa-2">
+      <div class="d-flex">
+        <img src="/src/assets/img/play-circle-outline.png" alt="hui" class="mr-1">
+        <div class="size16-weight700">LIVE</div>
+      </div>
+      <div class="flex-center">#190936</div>
+    </div>
+    <div class="card pa-5">
+      <div class="card-top text-align-center" @click="isUp = true">
+        <div>UP</div>
+        <div>1.73x Payout</div>
+      </div>
+      <div
+          class="card-content border"
+          :class="getBorderColor"
+      >
+        <div class="size12-weight700 mb-2">Last Price</div>
+        <div class="d-flex justify-space-between mb-4">
+          <div class="size24-weight700">$242.7848</div>
+          <div class="d-flex">
+            <img src="/src/assets/img/play-circle-outline.png" alt="hui" class="mr-1">
+            <div>$-0.0436</div>
+          </div>
+        </div>
+        <div class="d-flex justify-space-between size14-weight400">
+          <div>Locked Price:</div>
+          <div>$242.7934</div>
+        </div>
+        <div class="d-flex justify-space-between size16-weight700">
+          <div>Prize Pool:</div>
+          <div>9.4917 BNB</div>
+        </div>
+      </div>
+      <div class="card-bottom text-align-center" @click="isUp = false">
+        <div>1.73x Payout</div>
+        <div>DOWN</div>
+        <!--          <img src="/src/assets/img/figure-2.svg" alt="">-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardItem",
+  data() {
+    return {
+      isLoading: true,
+      isUp: true
+    };
+  },
+  computed: {
+    getBorderColor() {
+      return this.isUp ? "main-green-border" : "main-pink-border";
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.wrapper-block {
+  max-width: 320px;
+  width: 100%;
+  border-radius: 20px;
+
+  img {
+    width: 24px;
+  }
+}
+.card {
+  &-top {
+    background-image: url("/src/assets/img/figure-1.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    min-height: 65px;
+  }
+  &-content {
+    border-radius: 20px;
+    padding: 20px;
+    border-width: 3px;
+  }
+  &-bottom {
+    background-image: url("/src/assets/img/figure-2.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    min-height: 65px;
+  }
+}
+</style>
--- src/components/layout/DefaultLayout.vue
+++ src/components/layout/DefaultLayout.vue
@@ -1,14 +1,10 @@
 <template>
-  <div class="flex-space-between-column h-100-vh">
-    <div>
-<!--      <AppHeader />-->
-      <main class="bh-main">
-        <slot></slot>
-      </main>
-    </div>
-<!--    <AppFooter />-->
+  <div class="wrapper flex-center h-100 main-purple">
+    <CardItem class="mr-5"/>
+    <CardItem class="mr-5" />
+    <CardItem class="mr-5" />
+    <CardItem class="mr-5" />
   </div>
-<!--  <IndexModal />-->
 </template>
 
 <script lang="ts">
@@ -17,20 +13,42 @@ import { defineAsyncComponent, defineComponent, ref } from "vue";
 export default defineComponent({
   name: "DefaultLayout",
   components: {
-    // AppHeader: defineAsyncComponent(
-    //   () => import("@/components/common/AppHeader.vue")
-    // ),
-    // AppFooter: defineAsyncComponent(
-    //   () => import("@/components/common/AppFooter.vue")
-    // ),
-    // IndexModal: defineAsyncComponent(
-    //   () => import("@/components/modal/IndexModal.vue")
-    // ),
-  },
-  data() {
-    return {
-      isLoading: true,
-    };
+    CardItem: defineAsyncComponent(
+      () => import("@/components/CardItem.vue")
+    ),
   },
 });
 </script>
+
+<style scoped lang="scss">
+.wrapper-block {
+  max-width: 320px;
+  width: 100%;
+  border-radius: 20px;
+
+  img {
+    width: 24px;
+  }
+}
+.card {
+  &-top {
+    background-image: url("/src/assets/img/figure-1.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    min-height: 65px;
+  }
+  &-content {
+    border-radius: 20px;
+    padding: 20px;
+    border-width: 3px;
+  }
+  &-bottom {
+    background-image: url("/src/assets/img/figure-2.svg");
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+    min-height: 65px;
+  }
+}
+</style>
